9cf1c4d646bdc4f5f1c68d5cd044f8d92d74c7e9
Ateeq <ateeg1999@gmail.com>
2022-04-22 20:00:26 +0200

    Add vue 3 setup for the project

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Vue 3 App</h1>
+    {{ count }}
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue'
+
+    const count = ref(10)
+
+    function increment() {
+        count.value++
+    }
+
+    onMounted(() => {
+        console.log(`The initial count is ${count.value}.`)
+    })
+</script>
